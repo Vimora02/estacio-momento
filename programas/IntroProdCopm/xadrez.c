da32db8b057323959e56dcb5fc64a29c3d85d13f
Vimora02 <vimora02@gmail.com>
2025-05-14 15:36:05 -0300

    Desafio aventureiro
    Movimentação do cavalo adicionada

--- programas/IntroProdCopm/xadrez.c
+++ programas/IntroProdCopm/xadrez.c
@@ -3,20 +3,32 @@
 int main(){
 int t = 1, b = 1;
 //movimentação da torre
+printf("Torre:\n");
 while (t <= 5){
     printf("Direita\n");
     t++;
 }
 //movimentação do bispo
+printf("Bispo:\n");
 do{
     printf("Cima Direita\n");
     b++;
 } while (b<=5);
 
 //movimentação da rainha
+printf("Rainha:\n");
 for (int r = 1; r <= 8; r++){
     printf("Esquerda\n");
 }
-
+//movimentação do cavalo
+printf("Cavalo:\n");
+for (int c = 1; c <= 1; c++){
+    int c2 = 1;
+    while (c2 <= 2){
+        printf("Baixo\n");
+        c2++;
+    }
+    printf("Esquerda\n");
+}
 return 0;
 }
